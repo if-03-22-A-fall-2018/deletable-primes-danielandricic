5c6539bb6c6a016717fe25c5705df1be1259a164
danielandricic <dani.andricic@gmail.com>
2019-02-25 22:59:16 +0100

    Done, small performance problems at the last test.

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -1,12 +1,75 @@
+#include "deletable_primes.h"
+#include <math.h>
+
 /*----------------------------------------------------------
  *				HTBLA-Leonding / Class
  * ---------------------------------------------------------
- * Exercise Number: 0
+ * Exercise Number: 14
  * Title:			deletable_primes.c
- * Author:			P. Bauer
- * Due Date:		October 16, 2012
+ * Author:			D. Andricic
+ * Due Date:		25.Februar 2019
  * ----------------------------------------------------------
  * Description:
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+
+   unsigned long remove_digit(int index, unsigned long n)
+   {
+      long numPow = pow(10, index);
+      unsigned long num;
+      num = n / (numPow * 10) * numPow + (n % numPow);
+      return num;
+   }
+
+   bool is_prime(unsigned long p)
+   {
+     if (p <= 1 || (p % 2 == 0 && p > 2))
+     {
+       return false;
+     }
+
+     for(int i = 3; i <= sqrt(p); i += 2)
+     {
+       if (p % i == 0)
+       {
+         return false;
+       }
+     }
+     return true;
+   }
+
+   // Counter für das Iterieren.
+   int get_length(unsigned long n)
+   {
+     int count=0;
+
+     while (n > 1)
+     {
+       n /= 10;
+       count++;
+     }
+     return count;
+ }
+
+ int get_ways(unsigned long p)
+ {
+   if(p < 10 && is_prime(p))
+   {
+     return 1;
+   }
+   else if(p < 10 && !is_prime(p))
+   {
+     return 0;
+   }
+
+   int countways = 0;
+   for (int i = 0; i < get_length(p); i++)
+   {
+     if(is_prime(remove_digit(i, p)))
+     {
+       countways += get_ways(remove_digit(i,p));
+     }
+   }
+   return countways;
+  }
--- deletable_primes.h
+++ deletable_primes.h
@@ -13,4 +13,9 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
+unsigned long remove_digit(int index, unsigned long n);
+int get_ways(unsigned long num);
+bool is_prime(unsigned long num);
+int get_length(unsigned long num);
+
 #endif
